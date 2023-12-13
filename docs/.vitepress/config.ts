3ce49494da5639d6eb35b78d72e700c8e4f2c4c9
Reaper0x1 <reaper0x1@gmail.com>
2023-12-13 04:06:44 +0100

    update

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,6 +7,9 @@ export default defineConfig({
   description: 'Self hosted server wiki',
 
   themeConfig: {
+    search: {
+      provider: 'local'
+    },
     nav: [
       {
         text: 'Info',
