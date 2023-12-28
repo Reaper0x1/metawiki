e9f880db08f20eef75e0ab59fa58b665b4ee639a
ReaperX <81358777+Reaper0x1@users.noreply.github.com>
2023-12-28 16:20:36 +0000

    Refactor config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -23,10 +23,10 @@ export default defineConfig({
         text: "Proxmox",
         items: [
           { text: "Get started", link: "/proxmox/get-started" },
-          { text: "Installation", link: "/proxmox/installation" },
-          { text: "General configuration", link: "/proxmox/general-config" },
+          { text: "Install", link: "/proxmox/installation" },
+          { text: "Configuration", link: "/proxmox/general-config" },
           {
-            text: "External drive configuration",
+            text: "External Drive",
             link: "/proxmox/external-drive-configuration",
           },
           {
@@ -117,118 +117,112 @@ export default defineConfig({
 
     sidebar: [
       {
-        // text: 'Guide',
+        text: "Info",
+        link: "/info",
+      },
+      {
+        text: "Proxmox",
+        collapsed: false,
         items: [
+          { text: "Get started", link: "/proxmox/get-started" },
+          { text: "Install", link: "/proxmox/installation" },
           {
-            text: "Info",
-            link: "/info",
+            text: "Configuration",
+            link: "/proxmox/general-config",
           },
           {
-            text: "Proxmox",
-            collapsed: true,
-            items: [
-              { text: "Get started", link: "/proxmox/get-started" },
-              { text: "Installation", link: "/proxmox/installation" },
-              {
-                text: "General configuration",
-                link: "/proxmox/general-config",
-              },
-              {
-                text: "External drive configuration",
-                link: "/proxmox/external-drive-configuration",
-              },
-              {
-                text: "LXC Container",
-                collapsed: true,
-                items: [
-                  {
-                    text: "AdGuard Home",
-                    link: "/proxmox/lxc-container/adguardhome",
-                  },
-                  {
-                    text: "MariaDB",
-                    link: "/proxmox/lxc-container/mariadb",
-                  },
-                  {
-                    text: "Nginx Proxy Manager",
-                    link: "/proxmox/lxc-container/nginx-proxy-manager",
-                  },
-                  {
-                    text: "Wireguard",
-                    link: "/proxmox/lxc-container/wireguard",
-                  },
-                ],
-              },
-              { text: "Notes", link: "/proxmox/notes" },
-            ],
+            text: "External Drive",
+            link: "/proxmox/external-drive-configuration",
           },
           {
-            text: "Docker",
+            text: "LXC Container",
             collapsed: true,
             items: [
-              { text: "Install Docker", link: "/docker/docker.md" },
               {
-                text: "Docker Container",
-                collapsed: true,
-                items: [
-                  {
-                    text: "Authelia",
-                    link: "/docker/docker-container/authelia",
-                  },
-                  {
-                    text: "Authentik",
-                    link: "/docker/docker-container/authentik",
-                  },
-                  { text: "Deluge", link: "/docker/docker-container/deluge" },
-                  {
-                    text: "Fail2Ban",
-                    link: "/docker/docker-container/fail2ban",
-                  },
-                  {
-                    text: "FireflyIII",
-                    link: "/docker/docker-container/fireflyiii",
-                  },
-                  { text: "Gluetun", link: "/docker/docker-container/gluetun" },
-                  {
-                    text: "Homepage",
-                    link: "/docker/docker-container/homepage",
-                  },
-                  { text: "Netdata", link: "/docker/docker-container/netdata" },
-                  {
-                    text: "Nextcloud",
-                    link: "/docker/docker-container/nextcloud",
-                  },
-                  {
-                    text: "Nginx Proxy Manager",
-                    link: "/docker/docker-container/nginx-proxy-manager",
-                  },
-                  {
-                    text: "phpMyAdmin",
-                    link: "/docker/docker-container/phpmyadmin",
-                  },
-                  {
-                    text: "Plex Media Server",
-                    link: "/docker/docker-container/plex-media-server",
-                  },
-                  {
-                    text: "Prowlarr",
-                    link: "/docker/docker-container/prowlarr",
-                  },
-                  {
-                    text: "qBittorrent",
-                    link: "/docker/docker-container/qbittorrent",
-                  },
-                  { text: "Radarr", link: "/docker/docker-container/radarr" },
-                  { text: "VS Code", link: "/docker/docker-container/vs-code" },
-                ],
+                text: "AdGuard Home",
+                link: "/proxmox/lxc-container/adguardhome",
               },
               {
-                text: "Extra Configuration",
-                link: "/docker/extra-configuration.md",
+                text: "MariaDB",
+                link: "/proxmox/lxc-container/mariadb",
+              },
+              {
+                text: "Nginx Proxy Manager",
+                link: "/proxmox/lxc-container/nginx-proxy-manager",
+              },
+              {
+                text: "Wireguard",
+                link: "/proxmox/lxc-container/wireguard",
               },
             ],
           },
-          // ...
+          { text: "Notes", link: "/proxmox/notes" },
+        ],
+      },
+      {
+        text: "Docker",
+        collapsed: false,
+        items: [
+          { text: "Install Docker", link: "/docker/docker.md" },
+          {
+            text: "Docker Container",
+            collapsed: true,
+            items: [
+              {
+                text: "Authelia",
+                link: "/docker/docker-container/authelia",
+              },
+              {
+                text: "Authentik",
+                link: "/docker/docker-container/authentik",
+              },
+              { text: "Deluge", link: "/docker/docker-container/deluge" },
+              {
+                text: "Fail2Ban",
+                link: "/docker/docker-container/fail2ban",
+              },
+              {
+                text: "FireflyIII",
+                link: "/docker/docker-container/fireflyiii",
+              },
+              { text: "Gluetun", link: "/docker/docker-container/gluetun" },
+              {
+                text: "Homepage",
+                link: "/docker/docker-container/homepage",
+              },
+              { text: "Netdata", link: "/docker/docker-container/netdata" },
+              {
+                text: "Nextcloud",
+                link: "/docker/docker-container/nextcloud",
+              },
+              {
+                text: "Nginx Proxy Manager",
+                link: "/docker/docker-container/nginx-proxy-manager",
+              },
+              {
+                text: "phpMyAdmin",
+                link: "/docker/docker-container/phpmyadmin",
+              },
+              {
+                text: "Plex Media Server",
+                link: "/docker/docker-container/plex-media-server",
+              },
+              {
+                text: "Prowlarr",
+                link: "/docker/docker-container/prowlarr",
+              },
+              {
+                text: "qBittorrent",
+                link: "/docker/docker-container/qbittorrent",
+              },
+              { text: "Radarr", link: "/docker/docker-container/radarr" },
+              { text: "VS Code", link: "/docker/docker-container/vs-code" },
+            ],
+          },
+          {
+            text: "Extra Configuration",
+            link: "/docker/extra-configuration.md",
+          },
         ],
       },
     ],
